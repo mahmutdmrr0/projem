630f08f03e6131ccd52ad3c8ef8eeb3f5947a585
Mahmut Demir <mahmut.demir0024@gmail.com>
2025-04-22 15:05:48 +0300

    debug true ve hostu sıfırladım navbar bölümü düzenlenmeye çalışılıyor

--- projem/settings.py
+++ projem/settings.py
@@ -17,9 +17,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = []
 
 
 
