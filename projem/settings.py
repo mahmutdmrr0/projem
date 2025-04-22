9bf90c21a70dfc65155c170ffecce8120f5320d6
Mahmut Demir <mahmut.demir0024@gmail.com>
2025-04-22 13:42:28 +0300

    DEBUG False yapıldı, ALLOWED_HOSTS ayarlandı

--- projem/settings.py
+++ projem/settings.py
@@ -17,9 +17,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
 
 
 # Application definition
